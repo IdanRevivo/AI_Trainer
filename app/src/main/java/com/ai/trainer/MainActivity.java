c4d534c0ec1d1ca235927e9c1e8d983f0789b327
idanr@playtika.com <idanr@playtika.com>
2022-12-19 10:43:01 +0200

    Adding text field

--- app/src/main/java/com/ai/trainer/MainActivity.java
+++ app/src/main/java/com/ai/trainer/MainActivity.java
@@ -5,11 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void ClickFunction(View view){
-        Log.i("Info", "You've clicked the button");
+
+        EditText traineeName = (EditText) findViewById(R.id.userName);
+
+        EditText passwordText = (EditText) findViewById(R.id.passwordText);
+
+        Log.i("Values", traineeName.getText().toString());
+        Log.i("value", passwordText.getText().toString());
+        String popupMsg = "Hi " + traineeName.getText().toString();
+        Toast.makeText(this, popupMsg, Toast.LENGTH_SHORT).show();
     }
 
     @Override
